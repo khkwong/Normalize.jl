8ddad646cec3a5ed0e2d3bfc4e7cf7631ddbe393
Keith Kwong <chubbster7134@gmail.com>
2021-08-01 11:31:17 -0700

    change based on pull request comments

--- src/Normalize.jl
+++ src/Normalize.jl
@@ -3,6 +3,7 @@ module Normalize
 using Optim: optimize, minimizer
 using Statistics: mean, var
 using StatsBase: geomean
+using Distributions
 
 include("types.jl")
 include("boxcox.jl")
--- src/chisq.jl
+++ src/chisq.jl
@@ -1,6 +1,4 @@
-using StatsBase, Distributions
-
-function fitness(x::AbstractVector{T}) where {T <: Real}
+function chisq_goodness(x::AbstractVector{T}) where {T <: Real}
     # figure out number of bins, which depends on length(x)
     nbins = round(Int, ceil(2 * (length(x)^(2/5))))
 
@@ -9,16 +7,14 @@ function fitness(x::AbstractVector{T}) where {T <: Real}
 
     # use StatsBase `fit` function to count number of observations per bin
     bincounts = fit(Histogram, x, bin_range).weights
-    @info sum(bincounts)
 
     # use `pdf` function to find expected number of observations per bin
     bin_centers = bin_range[1:end-1] .+ bin_range.step.hi / 2
     expected_pdf = pdf.(Normal(0, 1), bin_centers)
-    bin_expected = expected_pdf .* sum(bincounts)/sum(expected_pdf)
+    bin_expected = expected_pdf .* sum(bincounts) / sum(expected_pdf)
 
     # calcuate pearson stats (X2)
     𝜒² = pearson_chi(bincounts, bin_expected)
-    @info bin_range bin_range.step.hi 𝜒² nbins
 
     # calculate X2/DF and return it
     return 𝜒² / nbins
@@ -29,7 +25,7 @@ function pearson_chi(
     expected::AbstractVector{T}
 ) where {T <: Integer}
     χ² = 0
-    for i=1:length(observed)
+    for i = 1:length(observed)
         χ² += (observed[i] - expected[i])^2 / expected[i]
     end
     return χ²
