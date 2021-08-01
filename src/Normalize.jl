6a4e94f5a095ef3d80aac6503bfb4b9cac034688
Keith Kwong <chubbster7134@gmail.com>
2021-08-01 11:54:45 -0700

    include only fit function from distribution pkg

--- src/Normalize.jl
+++ src/Normalize.jl
@@ -3,7 +3,7 @@ module Normalize
 using Optim: optimize, minimizer
 using Statistics: mean, var
 using StatsBase: geomean
-using Distributions
+using Distributions: fit
 
 include("types.jl")
 include("boxcox.jl")
